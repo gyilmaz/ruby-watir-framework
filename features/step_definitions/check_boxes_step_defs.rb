022a3602b9290e979949cf2612c4e0bfdae49582
archimedesphysics <guvenyilmaz@gmail.com>
2019-02-19 23:53:18 -0600

    Checkboxes step defs are completed

--- features/step_definitions/check_boxes_step_defs.rb
+++ features/step_definitions/check_boxes_step_defs.rb
@@ -4,20 +4,28 @@ When("I check all options by clicking a button") do
     page.input_forms
     page.checkboxes
   end
+  sleep 10
   on(CheckBoxesPage).btn_check_all
+  sleep 10
 end
 
 
 Then("I verify all boxes are checked and button name is {string}") do |string|
-#TODO
+ on(CheckBoxesPage) do |page|
+   expect(page.chk_option1_checked?).to be true
+   expect(page.chk_option2_checked?).to be true
+   expect(page.chk_option3_checked?).to be true
+   expect(page.chk_option4_checked?).to be true
+   expect(page.btn_element.text).to include("Uncheck All")
+ end
 end
 
 
 And("I unchecked one of the box") do
-#TODO
+    on(CheckBoxesPage).random_uncheck
 end
 
 
 Then("I verify button changed to {string}") do |string|
-#TODO
+  expect(on(CheckBoxesPage).btn_element.text).to include("Check All")
 end
--- lib/pages/check_boxes_page.rb
+++ lib/pages/check_boxes_page.rb
@@ -5,12 +5,21 @@ class CheckBoxesPage
   #button(:btn_check_all, id:'check1')
   button(:btn_check_all, value:'Check All' )
   button(:btn_uncheck_all, value:'Uncheck All' )
+  button(:btn, id:'check1')
 
-  checkbox(:chk_option1, value:'Option 1' )
-  checkbox(:chk_option2, value:'Option 2' )
-  checkbox(:chk_option3, value:'Option 3' )
-  checkbox(:chk_option4, value:'Option 4' )
+  checkbox(:chk_option1, xpath: "//label[contains(., 'Option 1' )]//child::input" )
+  checkbox(:chk_option2, xpath: "//label[contains(., 'Option 2' )]//child::input" )
+  checkbox(:chk_option3, xpath: "//label[contains(., 'Option 3' )]//child::input" )
+  checkbox(:chk_option4, xpath: "//label[contains(., 'Option 4' )]//child::input" )
 
-
+  def random_uncheck
+    @number= rand(0...5)
+    case @number
+    when 1 then uncheck_chk_option1
+    when 2 then uncheck_chk_option2
+    when 3 then uncheck_chk_option3
+    when 4 then uncheck_chk_option4
+    end
+  end
 
 end
